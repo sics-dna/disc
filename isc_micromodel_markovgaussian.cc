38c841165fae330c0a22202d41fcbb820126ac9e
tolATsicsDOTse <tol@sics.se>
2016-04-07 15:01:17 +0200

    Added multivarite gaussian directed graph implementation.

--- isc_micromodel_markovgaussian.cc
+++ isc_micromodel_markovgaussian.cc
@@ -22,171 +22,68 @@
 #include "isc_micromodel_markovgaussian.hh"
 
 
-virtual void IscMarkovGaussMicroModel::update_acc(IscMgdAccumulator* acc, intfloat* vec) {
-	double sum, sqsum, totcount, p, lp;
+virtual void IscMarkovGaussMicroModel::add_acc(IscMgdAccumulator *acc, intfloat* vec) {
+	double *tmp = new double[gdim_tot];
+	IscMgdGaussComp* gmod = getGaussComponent();
 
-	int category = static_cast<int>(vec[catind].i);
-	int index2;
-	IscMgdVector* mod1;
-	IscMgdGaussComp* mod2;
-	IscMgdGaussComp* mod2tmp;
-	lookup(vec, 0, mod1, mod2);
-	if (mod1) {
-		totcount = 1.0;
-		sum = modelinfo.global_sum / modelinfo.global_num;
-		sqsum = modelinfo.global_sqsum / modelinfo.global_num;
-		if (mod2) {
-			double targetcount = 0.0;
-			double *tmp = new double[gdim_tot];
-			for (int i=0; i<gdim_tot; i++)
-				tmp[i] = vec[gindv[i]].f - mod2->mean[i];
-			FORDIV(mod1->vector, mod2tmp, index2)
-			if (mod2tmp) {
-				/**/
-				double dist = mod2tmp->var->sqprodinv_part(tmp, gdim_predicted);
-				double det = mod2tmp->var->det_part(gdim_predicted);
-				double weightedcount = (det ? exp(-0.5*dist)/sqrt(det) : dist ? 0.0 : 1.0) * mod2tmp->count;
-				/**/
-				//            double weightedcount = mod2tmp->count;
-				double lc = log(weightedcount);
-				if (mod2 == mod2tmp) targetcount = weightedcount;
-				totcount += weightedcount;
-				sum += weightedcount * lc;
-				sqsum += weightedcount * lc * lc;
-			}
-			acc->numo += mod2->count;
-			acc->numc += 1.0;
-			acc->numd += 1.0;
-			acc->logp += log(targetcount/totcount);
-			acc->logpmean += sum/totcount - log(totcount);
-			acc->logpvar += sqsum/totcount - sum*sum/(totcount*totcount);
-			acc->sqdist += mod2->var->sqprodinv_cond(tmp, gdim_conditioned);
-			acc->logdet += log(mod2->var->det_cond(gdim_conditioned));
-			delete [] tmp;
-		} else {
-			// What to do if mod1 exists but we never jumped to mod2 before
-			FORDIV(mod1->vector, mod2tmp, index2)
-        		  if (mod2tmp) {
-        			  double weightedcount = mod2tmp->count;
-        			  double lc = log(weightedcount);
-        			  totcount += weightedcount;
-        			  sum += weightedcount * lc;
-        			  sqsum += weightedcount * lc * lc;
-        		  }
-			acc->numd += 1.0;
-			acc->logp += log(1.0/totcount);
-			acc->logpmean += sum/totcount - log(totcount);
-			acc->logpvar += sqsum/totcount - sum*sum/(totcount*totcount);
-		}
-	} else {
-		// What if we never were in mod1 before, and thus never jumped anywhere from there
-		int category = static_cast<int>(vec[catind].i);
-		double margnum;
-		if (category != -1)
-			mod1 = model[category];
-		if (mod1)
-			margnum = mod1->margin_num;
-		else
-			margnum = 1.0;
-		acc->numd += 1.0;
-		acc->logp += log(margnum/modelinfo.global_num);
-		acc->logpmean += modelinfo.global_sum / modelinfo.global_num - log(modelinfo.global_num);
-		acc->logpvar += modelinfo.global_sqsum / modelinfo.global_num - modelinfo.global_sum*modelinfo.global_sum / (modelinfo.global_num*modelinfo.global_num);
-	}
+	acc->sqdist += gmod->var->sqprodinv_cond(tmp, gdim_conditioned);
+	acc->logdet += log(gmod->var->det_cond(gdim_conditioned));
+	delete [] tmp;
+
 };
 
 
 // Training
 virtual void IscMarkovGaussMicroModel::add(intfloat* vec) {
 	double *tmp = new double[gdim_tot];
-	double lc1, lc0;
-	IscMgdVector* mod1;
-	IscMgdGaussComp* mod2;
-	lookup(vec, 1, mod1, mod2);
-	if (mod2) {
-		for (int i=0; i<gdim_tot; i++)
-			tmp[i] = vec[gindv[i]].f - mod2->mean[i];
-		mod2->count += 1.0;
-		for (int i=0; i<gdim_tot; i++)
-			mod2->mean[i] += tmp[i]/mod2->count;
-		mod2->var->accum(tmp, 1.0/mod2->count);
-		mod2->var->scale((mod2->count - 1.0)/mod2->count);
-		lc0 = (mod1->margin_num ? log(mod1->margin_num) : 0.0);
-		mod1->margin_num += 1.0;
-		lc1 = (mod1->margin_num ? log(mod1->margin_num) : 0.0);
-		modelinfo.global_num += 1.0;
-		modelinfo.global_sum += mod1->margin_num*(lc1-lc0) + lc0;
-		modelinfo.global_sqsum += mod1->margin_num*(lc1*lc1-lc0*lc0) + lc0*lc0;
-	}
+	IscMgdGaussComp* gmod = getGaussComponent();
+
+	for (int i=0; i<gdim_tot; i++)
+		tmp[i] = vec[gindv[i]].f - mod2->mean[i];
+	gmod->count += 1.0;
+	for (int i=0; i<gdim_tot; i++)
+		gmod->mean[i] += tmp[i]/gmod->count;
+	gmod->var->accum(tmp, 1.0/gmod->count);
+	gmod->var->scale((gmod->count - 1.0)/gmod->count);
+
 	delete [] tmp;
 };
 
 virtual void IscMarkovGaussMicroModel::remove(intfloat* vec) {
 	double *tmp = new double[gdim_tot];
 	double lc0, lc1;
-	IscMgdVector* mod1;
-	IscMgdGaussComp* mod2;
-	lookup(vec, 1, mod1, mod2);
-	if (mod2) {
-		for (int i=0; i<gdim_tot; i++)
-			tmp[i] = vec[gindv[i]].f - mod2->mean[i];
-		mod2->count -= 1.0;
-		for (int i=0; i<gdim_tot; i++)
-			mod2->mean[i] -= tmp[i]/mod2->count;
-		mod2->var->accum(tmp, -1.0/mod2->count);
-		mod2->var->scale((mod2->count + 1.0)/mod2->count);
-		lc0 = (mod1->margin_num ? log(mod1->margin_num) : 0.0);
-		mod1->margin_num -= 1.0;
-		lc1 = (mod1->margin_num ? log(mod1->margin_num) : 0.0);
-		modelinfo.global_num -= 1.0;
-		modelinfo.global_sum += mod1->margin_num*(lc1-lc0) - lc0;
-		modelinfo.global_sqsum += mod1->margin_num*(lc1*lc1-lc0*lc0) - lc0*lc0;
-	}
+	IscMgdGaussComp* gmod = getGaussComponent();
+
+	for (int i=0; i<gdim_tot; i++)
+		tmp[i] = vec[gindv[i]].f - mod2->mean[i];
+	gmod->count -= 1.0;
+	for (int i=0; i<gdim_tot; i++)
+		gmod->mean[i] -= tmp[i]/mod2->count;
+	gmod->var->accum(tmp, -1.0/mod2->count);
+	gmod->var->scale((gmod->count + 1.0)/gmod->count);
+
 	delete [] tmp;
 };
 
 virtual void IscMarkovGaussMicroModel::reset() {
-	IscMgdVector* mod1;
-	IscMgdGaussComp* mod2;
-	int index1, index2;
-	FORDIV(model, mod1, index1)
-		if (mod1) {
-			mod1->margin_num = 0.0;
-			FORDIV(mod1->vector, mod2, index2)
-				if (mod2) {
-					mod2->count = modelinfo.prior_n;
-					for (int i=0; i<gdim_tot; i++)
-						mod2->mean[i] = 0.0;
-					mod2->var->unit();
-					mod2->var->scale(modelinfo.prior_v);
-				}
-		}
+
+	IscMgdGaussComp* gmod = getGaussComponent();
+	gmod->count = modelinfo.prior_n;
+	for (int i=0; i<gdim_tot; i++)
+		gmod->mean[i] = 0.0;
+	gmod->var->unit();
+	gmod->var->scale(modelinfo.prior_v);
+
 	modelinfo.reset();
 }
 
 
-void IscMarkovGaussMicroModel::lookup(intfloat* vec, int insert, IscMgdVector*& mod1, IscMgdGaussComp*& mod2) {
-	int category = static_cast<int>(vec[catind].i);
-	if (category != -1) {
-		mod1 = model[category];
-		if (mod1) {
-			mod2 = mod1->vector[0];
-			if (!mod2 && insert)
-				mod2 = mod1->vector[0] = new IscMgdGaussComp(gdim_tot, modelinfo.prior_n, modelinfo.prior_v);
-		} else if (insert) {
-			mod1 = model[category] = new IscMgdVector();
-			mod2 = mod1->vector[0] = new IscMgdGaussComp(gdim_tot, modelinfo.prior_n, modelinfo.prior_v);
-		} else {
-			mod1 = 0;
-			mod2 = 0;
-		}
-		return;
-	} else {
-		mod1 = 0;
-		mod2 = 0;
-		return;
+virtual IscMgdGaussComp* IscMarkovGaussMicroModel::getGaussComponent() {
+	if(!mod2) {
+		gaussian_component = new IscMgdGaussComp(gdim_tot, modelinfo.prior_n, modelinfo.prior_v);
 	}
-};
+	return gaussian_component;
+}
 
 
 /*
--- isc_micromodel_markovgaussian.hh
+++ isc_micromodel_markovgaussian.hh
@@ -1,4 +1,4 @@
- /*
+/*
  --------------------------------------------------------------------------
  Copyright (C) 2015 SICS Swedish ICT AB
 
@@ -38,147 +38,184 @@
 #include "intfloat.hh"
 
 
-// A Markov chain of a multivariate Gaussian distribution.
+// A Markov model of a multivariate Gaussian distribution.
 // That is, the state is described by a
-// set of continuous values from a multivariate Gaussian for each discrete number. 
+// set of continuous values from a multivariate Gaussian
 
 struct IscMgdGaussComp {
-  IscMgdGaussComp(int dim, double prior_n, double prior_v) {
-    count = prior_n;
-    mean = new double[dim];
-    for (int i=0; i<dim; i++)
-      mean[i] = 0.0;
-    var = new HMatrix(dim, 2);
-    var->unit();
-    var->scale(prior_v);
-  };
-  double count;
-  double* mean;
-  HMatrix* var;
+	IscMgdGaussComp(int dim, double prior_n, double prior_v) {
+		count = prior_n;
+		mean = new double[dim];
+		for (int i=0; i<dim; i++)
+			mean[i] = 0.0;
+		var = new HMatrix(dim, 2);
+		var->unit();
+		var->scale(prior_v);
+	};
+	double count;
+	double* mean;
+	HMatrix* var;
 };
 
-struct IscMgdVector {
-  IscMgdVector() : vector(0) { margin_num = 0.0; };
-  DynamicIndexVector<IscMgdGaussComp*> vector;
-  double margin_num;
-};
 
 struct IscMgdGlobalInfo {
-  IscMgdGlobalInfo() { prior_n = prior_v = prior_cellcount = global_num = global_sum = global_sqsum = 0.0; };
-  void reset() { global_num = prior_cellcount; global_sum = global_sqsum = 0.0; };
-  double prior_n, prior_v;
-  double prior_cellcount;
-  double global_num;
-  double global_sum;
-  double global_sqsum;
+	IscMgdGlobalInfo() { prior_n = prior_v =  0.0; };
+	void reset() { };
+	double prior_n, prior_v;
 };
 
-struct IscMgdAccumulator {
-  IscMgdAccumulator() { numo = numc = numd = sqdist = logdet = logp = logpmean = logpvar = 0.0; };
-  double numo;
-  double numc;
-  double numd;
-  double sqdist;
-  double logdet;
-  double logp;
-  double logpmean;
-  double logpvar;
+class IscMgdAccumulator {
+	IscMgdAccumulator() {sqdist = logdet = 0.0; };
+
+
+	// Read out anomaly and log predicted prob
+	virtual double anomaly_acc() {
+		//double ddist = (numd == 0 ? 0.0 : (logp - logpmean)*(logp - logpmean)/logpvar);
+		//return -logintmultistudent(0.5*(5*(numo+numd)/(numc+1.0)), sqrt((sqdist*0.5 + ddist*0.67)/(5*(numo+numd)/(numc+1.0))), numc/* +1.0-1.0*/);
+		return -log(0.5*incgammaq(0.5, (sqdist*0.5))); // + ddist*0.67)));
+		//return (logp < logpmean ? -log(0.5*incgammaq(0.5, ddist*0.5)) + 3.0 : 0.0);
+	};
+
+	virtual double logp_acc() {
+		// just a gaussian for now, should be multi-student-t
+		return - 0.5*sqdist - 0.5*logdet;
+	};
+
+
+	virtual void reset_acc() {
+		sqdist = 0.0;
+		logdet = 0.0;
+	};
+
+	double sqdist;
+	double logdet;
+
+	IscMgdGlobalInfo modelinfo;
 };
 
 class IscMarkovGaussMicroModel : public IscMicroModel {
 
 public:
-  // predicted_variable_index contains the indices in the vector of the features of this micromodel.
-  // that are predicted and conditional_variable_index_length contains the indices in the vector of the features
-  // in the condition clause: p(<predicted variables> | <conditional variables>)
-  //
-  IscMarkovGaussMicroModel(
-		  int* predicted_variable_index,
-		  int predicted_variable_length,
-		  int* conditional_variable_index,
-		  int conditional_variable_length,
-		  int category_index
-		  ) : model(0) {
-    gdim_predicted = predicted_variable_length;
-    gdim_conditioned = conditional_variable_length;
-	gdim_tot = gdim_predicted+gdim_conditioned;
+	// predicted_variable_index contains the indices in the vector of the features of this micromodel.
+	// that are predicted and conditional_variable_index_length contains the indices in the vector of the features
+	// in the condition clause: p(<predicted variables> | <conditional variables>)
+	//
+	IscMarkovGaussMicroModel(
+			int* predicted_variable_index,
+			int predicted_variable_length,
+			int* conditional_variable_index,
+			int conditional_variable_length
+	)  {
+		gdim_predicted = predicted_variable_length;
+		gdim_conditioned = conditional_variable_length;
+		gdim_tot = gdim_predicted+gdim_conditioned;
 
 
-    gindv = new int[gdim_tot];
-    catind = category_index;
-    for (int i=0; i<gdim_predicted; i++) {
-      gindv[i] = predicted_variable_index[i];
-    }
-    for (int i=gdim_predicted; i<gdim_predicted+gdim_predicted; i++) {
-      gindv[i] = conditional_variable_index[i];
-    }
+		gindv = new int[gdim_tot];
+		for (int i=0; i<gdim_predicted; i++) {
+			gindv[i] = predicted_variable_index[i];
+		}
+		for (int i=gdim_predicted; i<gdim_predicted+gdim_predicted; i++) {
+			gindv[i] = conditional_variable_index[i];
+		}
+	};
+	virtual ~IscMarkovGaussMicroModel() { delete [] gindv; };
 
-  };
-  virtual ~IscMarkovGaussMicroModel() { delete [] gindv; };
 
-  virtual void reset_acc(IscMgdAccumulator* acc) {
-    acc->numo = 0.0;
-    acc->numc = 0.0;
-    acc->numd = 0.0;
-    acc->sqdist = 0.0;
-    acc->logdet = 0.0;
-    acc->logp = 0.0;
-    acc->logpmean = 0.0;
-    acc->logpvar = 0.0;
-  };
+	virtual void add_acc(IscMgdAccumulator *acc, intfloat* vec);
 
-  virtual void update_acc(IscMgdAccumulator* acc, intfloat* vec);
+	virtual double anomaly(intfloat* vec) {
+		IscMgdAccumulator* acc = new IscMgdAccumulator();
+		add_acc(acc,vec);
+		return acc->anomaly_acc();
+	};
+	virtual double logp(intfloat* vec) {
+		IscMgdAccumulator* acc = new IscMgdAccumulator();
+		add_acc(acc,vec);
+		return acc->logp_acc();
+	};
 
-  // Read out anomaly and log predicted prob
-  virtual double anomaly_acc(IscMgdAccumulator* acc) {
-    double ddist = (acc->numd == 0 ? 0.0 : (acc->logp - acc->logpmean)*(acc->logp - acc->logpmean)/acc->logpvar);
-    //return -logintmultistudent(0.5*(5*(acc->numo+acc->numd)/(acc->numc+1.0)), sqrt((acc->sqdist*0.5 + ddist*0.67)/(5*(acc->numo+acc->numd)/(acc->numc+1.0))), acc->numc/* +1.0-1.0*/); 
-    return -log(0.5*incgammaq(0.5, (acc->sqdist*0.5 + ddist*0.67)));
-    //return (acc->logp < acc->logpmean ? -log(0.5*incgammaq(0.5, ddist*0.5)) + 3.0 : 0.0);
-  };
+	// NOT IMPLEMENTED
+	virtual int ev_logp(intfloat* vec, double& e, double& v) { return 0; };
+	virtual int logpeak(intfloat* vec, double& p) { return 0; };
+	virtual int invanomaly(intfloat* vec, double ano, intfloat* minvec, intfloat* maxvec, intfloat* peakvec) { return 0; };
+	virtual int stats(union intfloat* vec, double* expect, double* var) { return 0; };
 
-  virtual double logp_acc(IscMgdAccumulator* acc) {
-// just a gaussian for now, should be multi-student-t
-    return acc->logp - 0.5*acc->sqdist - 0.5*acc->logdet;
-  };
+	// Training
+	virtual void add(intfloat* vec);
 
-  virtual double anomaly(intfloat* vec) { return 0.0; };
-  virtual double logp(intfloat* vec) { return 0.0; };
+	virtual void remove(intfloat* vec);
 
-  virtual int ev_logp(intfloat* vec, double& e, double& v) { return 0; };
-  virtual int logpeak(intfloat* vec, double& p) { return 0; };
-  virtual int invanomaly(intfloat* vec, double ano, intfloat* minvec, intfloat* maxvec, intfloat* peakvec) { return 0; };
-  virtual int stats(union intfloat* vec, double* expect, double* var) { return 0; };
+	virtual void reset();
 
-  // Training
-  virtual void add(intfloat* vec);
-
-  virtual void remove(intfloat* vec);
-
-  virtual void reset();
-
-  virtual void set_prior(double nn, double vv, double cc) { modelinfo.prior_n = nn; modelinfo.prior_v = vv; modelinfo.prior_cellcount = cc; };
-
-  virtual bool has_matching_link(intfloat* vec) {
-    IscMgdVector* mod1;
-    IscMgdGaussComp* mod2;
-    lookup(vec, 0, mod1, mod2);
-    return (mod2 ? true : false);
-  }
+	virtual void set_prior(double nn, double vv) {
+		modelinfo.prior_n = nn;
+		modelinfo.prior_v = vv;
+	};
+	virtual IscMgdGaussComp* getGaussComponent();
 
 protected:
 
-  virtual void lookup(intfloat* vec, int insert, IscMgdVector*& mod1, IscMgdGaussComp*& mod2);
-  int gdim_tot, gdim_predicted, gdim_conditioned;
-  int catind;
-  int* gindv;
-  IscMgdGlobalInfo modelinfo;
-  DynamicIndexVector<IscMgdVector*> model;
+	int gdim_tot, gdim_predicted, gdim_conditioned;
+	int* gindv;
+	IscMgdGlobalInfo modelinfo;
+	IscMgdGaussComp* gaussian_component;
 };
 
-/*
- En dynindvec, nej hash, med "g1:g2:type" som nyckel, innehåll: count, mean, var.
- Eller en dynindvec med geo1*20+type, av dyninvec geo2, av  count, mean, var.
-*/
+//
+// IscMarkovGaussCombinerMicroModel takes as input an array of IscMarkovGaussMicroModels
+// and combines them into one model.It only works if the IscMarkovGaussMicroModels are
+// (conditional) independent. This is equivalent to a directed graph model, e.g.,
+// a Bayesian Network.
+//
+class IscMarkovGaussCombinerMicroModel : public IscMicroModel {
+
+	IscMarkovGaussCombinerMicroModel(IscMarkovGaussMicroModel* gaussian_components, int num_of_components) {
+		this->gaussian_components=gaussian_components;
+		this->num_of_components=num_of_components;
+		this->accumulator = new IscMgdAccumulator();
+	};
+	~IscMarkovGaussCombinerMicroModel();
+
+	// Read out anomaly and log predicted prob
+	virtual double anomaly(union intfloat* vec) {
+		accumulator->reset_acc();
+		for(int i=0; i < num_of_components; i++) {
+			gaussian_components[i].add_acc(accumulator,vec);
+		}
+		return accumulator->anomaly_acc();
+	};
+	virtual double logp(union intfloat* vec) {
+		accumulator->reset_acc();
+		for(int i=0; i < num_of_components; i++) {
+			gaussian_components[i].add_acc(accumulator,vec);
+		}
+		return accumulator->logp_acc();
+	}
+
+	// Training
+	virtual void add(union intfloat* vec) {
+		for(int i=0; i < num_of_components; i++) {
+			gaussian_components[i].add(vec);
+		}
+	};
+
+	virtual void remove(union intfloat* vec) {
+		for(int i=0; i < num_of_components; i++) {
+			gaussian_components[i].remove(vec);
+		}
+	}
+	virtual void reset() {
+		for(int i=0; i < num_of_components; i++) {
+			gaussian_components[i].reset();
+		}
+	}
+protected:
+	IscMarkovGaussMicroModel* gaussian_components;
+	int num_of_components;
+	IscMgdAccumulator* accumulator;
+};
+
+
 #endif
 // End ISC_MICROMODEL_MARKOVGAUSSIAN_HH_
